d2dd27b1da2e475093bf5811d91dd43d38d74fdd
Karczykowski <115210784+Karczykowski@users.noreply.github.com>
2024-01-21 21:07:10 +0100

    uninteractable button on game off

--- Assets/Scripts/MonsterSelector.cs
+++ Assets/Scripts/MonsterSelector.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class MonsterSelector : MonoBehaviour
 {
@@ -10,9 +12,15 @@ public class MonsterSelector : MonoBehaviour
     [SerializeField] private List<Monster> playerMonsters;
     [SerializeField] private List<Monster> enemyMonsters;
 
+    [SerializeField] private Button attackButton;
+    [SerializeField] private Button otherButton;
+
+
     public void StartGame()
     {
         playerStation.SendMonster(Instantiate(playerMonsters[0], Vector2.zero, Quaternion.identity));
         enemyStation.SendMonster(Instantiate(enemyMonsters[0], Vector2.zero, Quaternion.identity));
+        attackButton.interactable = true;
+        otherButton.interactable = true;
     }
 }
